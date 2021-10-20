74404339d22b624265e95954dfa4b80195f5b150
Alessandro <par_214@hotmail.com>
2021-10-20 18:23:32 -0500

    Restored process.env

--- src/services/common.js
+++ src/services/common.js
@@ -1,5 +1,5 @@
 const fetchAPI = async (route, method = 'GET',  { headers, body, params } = {}) => {
-  const baseURL = "http://localhost:3000"
+  const baseURL = process.env.VUE_APP_API_URL //Please do not remove this!
   const response = await fetch(`${baseURL + route}`, {
     method: method,
     headers: {
