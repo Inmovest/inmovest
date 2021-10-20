d5991eafdc0fbddef93c87615b2861bd73ae5820
Alessandro Chumpitaz <par_214@hotmail.com>
2021-10-20 17:53:19 -0500

    Added Developer Views and Investments Table, little Refactor

--- src/services/profilesService.js
+++
@@ -1,5 +0,0 @@
-import fetchAPI from "@/services/common";
-const getUserById = async (id) => fetchAPI(`/users/${id}?`)
-const updateUserInfo = async (id, body) => fetchAPI('/users/${id}?', 'POST', { body: body });
-
-export { getUserById, updateUserInfo }
