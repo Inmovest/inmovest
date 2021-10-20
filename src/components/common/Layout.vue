09df6cfb55d3052f547d95e6a025588aefa53986
EnzoLivelli2000 <enzo.livelli@gmail.com>
2021-10-19 19:29:59 -0500

    Realization of the notification display interface

--- src/components/common/Layout.vue
+++ src/components/common/Layout.vue
@@ -15,6 +15,13 @@
             <v-app-bar-title>Inmovest</v-app-bar-title>
           </div>
         </router-link>
+        <v-spacer></v-spacer>
+
+        <router-link to="/user/1/messages">
+          <v-btn icon class= "ml-1 p-5">
+            <v-icon>mdi-message</v-icon>
+          </v-btn>
+        </router-link>
       </v-app-bar>
     </nav>
     <v-navigation-drawer
