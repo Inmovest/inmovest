2dfa241098a6aebcee7389e9edad96a47ded296d
Alessandro <par_214@hotmail.com>
2021-10-22 23:11:13 -0500

    Added Payment History View

--- src/views/users/Payments.vue
+++ src/views/users/Payments.vue
@@ -1,19 +1,50 @@
 <template>
   <Layout>
-    <h1>sda</h1>
+    <div class="w-full">
+      <h1>Payment History</h1>
+      <v-data-table
+          dense
+          :headers="dataSource.headers"
+          :items="dataSource.payments"
+          item-key="id"
+      >
+        <template v-slot:item.amount="{ item }">
+          S/. {{ item.amount }}
+        </template>
+      </v-data-table>
+    </div>
   </Layout>
 </template>
 
 <script>
 import Layout from "@/components/common/Layout";
+import { getPayments } from "@/services/user/paymentsService";
 export default {
   name: "Payments.vue",
   components: {
     Layout
+  },
+  data() {
+    return {
+      payments: [],
+      loading: false,
+      error: null,
+      dataSource: {
+        headers: [
+          { text: 'Identifier', value: 'id' },
+          { text: 'Amount', value: 'amount', sortable: true },
+          { text: 'Comments', value: 'comment' }
+        ],
+        payments: [],
+        loading: false,
+        error: null
+      }
+    }
+  },
+  beforeMount() {
+    getPayments()
+        .then(data => this.$data.dataSource.payments = data)
+        .catch(e => this.$data.dataSource.error = e)
   }
 }
 </script>
-
-<style scoped>
-
-</style>
