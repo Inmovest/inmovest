9f0dd90e3fb5895d3086347e43efb3e5df9769e1
Alessandro Chumpitaz <par_214@hotmail.com>
2021-10-22 22:36:51 -0500

    Added some Payments

---
+++ src/views/users/Payments.vue
@@ -0,0 +1,19 @@
+<template>
+  <Layout>
+    <h1>sda</h1>
+  </Layout>
+</template>
+
+<script>
+import Layout from "@/components/common/Layout";
+export default {
+  name: "Payments.vue",
+  components: {
+    Layout
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
