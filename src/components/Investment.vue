29a533996b642711f92c0ccde7330488238f82c0
JeffersonHGaramendi <jeffersonluishg2@gmail.com>
2021-10-23 03:08:32 -0500

    State of investment & Delete investment implemented

--- src/components/Investment.vue
+++ src/components/Investment.vue
@@ -18,6 +18,41 @@
           <p class="text-sm">{{investment.sentAt.substring(0,8)}}</p>
           <p class="text-sm">Time: {{investment.sentAt.substring(8)}}</p>
         </div>
+        <div class="col flex items-center">
+          <p>State: Active</p>
+        </div>
+        <div class="col flex items-center">
+        <v-card-actions>
+          <v-dialog v-model="dialog" width="500">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn color="error" dark v-bind="attrs" v-on="on">
+                <v-icon>mdi-delete</v-icon>Delete
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title class="text-h5 grey lighten-2">
+                Eliminar inversión
+              </v-card-title>
+
+              <v-card-text class="mt-5">
+                ¿Estás seguro que quieres eliminar la inversión a este proyecto?
+              </v-card-text>
+
+              <v-divider></v-divider>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" text @click="dialog = false">
+                  Eliminar
+                </v-btn>
+                <v-btn color="primary" depressed @click="dialog = false">
+                  Cancelar
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-card-actions>
+        </div>
       </div>
     </v-card>
   </v-hover>
